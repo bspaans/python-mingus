e1a5abaf4add574ffabde18c664697a15bd188a0
Rhijnauwen <devnull@localhost>
2008-09-07 05:26:47 +0000

    Added everything from the documentation

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+#	mingus build script
+#	Builds mingus, registers at PyPi, generates documentation and uploads to Google code
+
+VERSION=`cat ~/mingus/setup.py | grep version | awk -F = '{print $2}' | awk -F \" '{print $2}'`
+
+echo "*******************************************************************************"
+echo "*****                     mingus :: Automatic Build                       *****"                     
+echo "*****                  Building mingus version $VERSION                   *****"
+echo "*******************************************************************************"
+echo
+echo "              Generate Documentation and Distribute to Google Code"
+echo
+echo "*******************************************************************************"
+sudo rm -r ./build/ && sudo python setup.py install
+~/mingus/doc/distribute_docs.sh
+echo "*******************************************************************************"
+echo
+echo "                 Registering package mingus-$VERSION at PyPi"
+echo
+echo "*******************************************************************************"
+python setup.py register
+echo "*******************************************************************************"
+echo
+echo "          Uploading source distribution and windows installer to PyPi"
+echo
+echo "*******************************************************************************"
+sudo python setup.py sdist bdist_wininst upload
+echo "*******************************************************************************"
+echo
+echo "              Uploading source and windows installer to Google Code"
+echo
+echo "*******************************************************************************"
+echo "Uploading windows installer"
+python googlecode_upload.py -p mingus -s "mingus-$VERSION - windows installer" -u rhijnauwen --config-dir=/home/bspaans/coding/python/mingus-gc-wiki/ -l Featured /home/bspaans/coding/python/mingus/dist/mingus-$VERSION.win32.exe
+
+echo
+echo "Uploading source distribution"
+python googlecode_upload.py -p mingus -s "mingus-$VERSION - source tarball" -u rhijnauwen --config-dir=/home/bspaans/coding/python/mingus-gc-wiki/ -l Featured /home/bspaans/coding/python/mingus/dist/mingus-$VERSION.tar.gz
---
+++ doc/distribute_docs.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# mingus 0.2
+# This little scripts checks the wiki documentation into google
+
+echo mingus 0.2 - Generate wiki documentation and upload to googlecode
+echo
+echo
+echo =========================Generating wiki documentation=========================
+echo
+
+~/mingus/doc/generate_wiki_docs.py &&
+mv ~/mingus/doc/*.wiki ~/coding/python/mingus-gc-wiki &&
+
+echo 
+echo ===========================Listing repository info=============================
+echo
+
+cd ~/coding/python/mingus-gc-wiki &&
+svn info &&
+
+echo
+echo =====================Adding .wiki files to the repository======================
+echo
+
+svn add ref*.wiki &&
+
+echo
+echo =============================Commit Changes====================================
+echo
+
+svn status -q &&
+svn ci -m "Automated upload of reference docs"
+
+echo
+echo Done
