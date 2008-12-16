d3c8114f1d058991d2973ee29edb074eab191dcb
Rhijnauwen <devnull@localhost>
2008-12-16 02:11:00 +0000

    Added examples to the setup script

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from distutils.core import setup
 
 setup(name= "mingus",
-	  version = "0.3.2.2",
+	  version = "0.3.2.3",
 	  description = "mingus is an advanced music theory and notation package",
 	  long_description = "mingus is an advanced music theory and notation package "\
 			"for Python with MIDI playback support. It can be used to play "\
 			"around with music theory, to build editors, educational tools and "\
 			"other applications that need to process and/or play music. "\
 			"It can also be used to create sheet music with LilyPond "\
-			"and do automated musicological analysis.",
+			"and do automated musicological analysis on chords, scales and "\
+			"harmonic progressions.",
 	  author = "Bart Spaans",
 	  author_email = "onderstekop@gmail.com",
 	  url = "http://mingus.googlecode.com/",
 	  packages = ['mingus', 'mingus.core', 'mingus.containers', 'mingus.extra'],
+	  data_files = [('mingus_examples/pygame-drum/', ['mingus_examples/pygame-drum/pad.png',\
+			  		     'mingus_examples/pygame-drum/pygame-drum.py']),\
+			('mingus_examples/pygame-piano/', ['mingus_examples/pygame-piano/pygame-piano.py',
+				 			   'mingus_examples/pygame-piano/keys.png'])],
 	  classifiers = [
 			'Intended Audience :: Developers',
 			'Intended Audience :: Science/Research',
