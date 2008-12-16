e0f639ae1e2e41c41ac83c8a192e74ac20b32aba
Rhijnauwen <devnull@localhost>
2008-12-16 05:53:23 +0000

    Added drums to the improviser

--- mingus_examples/improviser/improviser.py
+++ mingus_examples/improviser/improviser.py
@@ -31,6 +31,7 @@ if not fluidsynth.init_fluidsynth():
 	print "Couldn't connect to FluidSynth server at port 9800."
 	sys.exit(1)
 
+loop = 1
 while 1:
 	i = 0 
 	for chord in chords:
@@ -44,10 +45,12 @@ while 1:
 
 		# Play chord and lowered first note
 		fluidsynth.play_NoteContainer(c, randrange(50,75), 1)
+		if loop % 2 == 0:
+			fluidsynth.play_NoteContainer(c, randrange(50,75), 5)
 		fluidsynth.play_Note(l, randrange(50,75), 2)
 
 		# Create random solo over chord
-		beats = [ random() > 0.5 for x in range(8)]
+		beats = [ random() > 0.5 for x in range((loop % 2 + 1) * 8)]
 		t = 0
 		for beat in beats:
 			if beat:
@@ -61,14 +64,31 @@ while 1:
 						n = Note(intervals.seventh(choice(c).name, key))
 				fluidsynth.play_Note(n, randrange(80, 110), 11)
 				print n
-			if t == 5:
+			if t % (len(beats) / 2) == 0 and t != 0:
 				fluidsynth.play_NoteContainer(c, randrange(50, 75), 7)
+
+			# Drums
+			if loop > 0:
+				if t % (len(beats) / 2) == 0 and t != 0:
+					fluidsynth.play_Note(Note("E", 2), 100, 9)
+				else:
+					if random() > 0.8 or t == 0:
+						fluidsynth.play_Note(Note("C", 2), 100, 9)
+
+				if t % 2 == 0: 
+					fluidsynth.play_Note(Note("A#", 2), 100, 9)
+				else:
+					if random() > 0.9:
+						fluidsynth.play_Note(Note("E", 2), 100, 9)
+						
 
 			time.sleep( 2.0 / len(beats))
 			t += 1
 
 		fluidsynth.stop_NoteContainer(c, 1)
+		fluidsynth.stop_NoteContainer(c, 5)
 		fluidsynth.stop_Note(l, 2)
 		fluidsynth.stop_Note(n, 11)
 		i += 1
 	print "-" * 20
+	loop += 1
