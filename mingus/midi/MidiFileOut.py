935bef85f785427d4b571ff066d0f5098d722073
Rhijnauwen <devnull@localhost>
2008-12-28 07:10:41 +0000

    Moved the MidiFile class to MidiFileOut.

--- mingus/midi/MidiFileOut.py
+++ mingus/midi/MidiFileOut.py
@@ -27,8 +27,52 @@
 
 """
 from MidiTrack import MidiTrack
-from MidiFile import MidiFile
+from binascii import a2b_hex
 
+class MidiFile():
+	"""This class generates midi files from MidiTracks. """
+
+	tracks = []
+	time_division = "\x00\x48"
+
+	def __init__(self, tracks = []):
+		self.reset()
+		self.tracks = tracks
+
+	def get_midi_data(self):
+		"""Collects and returns the raw, binary MIDI data
+		from the tracks."""
+		tracks = [ t.get_midi_data() for t in self.tracks if \
+				t.track_data != '']
+		return self.header() + "".join(tracks)
+
+	def header(self):
+		"""Returns a header for type 1 midi file"""
+		tracks = a2b_hex("%04x" % len([ t for t in self.tracks if\
+						t.track_data != '']))
+		return "MThd\x00\x00\x00\x06\x00\x01" + tracks + self.time_division
+
+	def reset(self):
+		"""Resets every track."""
+		[ t.reset() for t in self.tracks ]
+
+	def write_file(self, file):
+		"""Collects the data from get_midi_data and writes to file. """
+		"""Returns True on success. False on failure."""
+		dat = self.get_midi_data()
+		try:
+			f = open(file, "wb")
+		except:
+			print "Couldn't open '%s' for writing." % file
+			return False
+		try:
+			f.write(dat)
+		except:
+			print "An error occured while writing data to %s." % file
+			return False
+		f.close()
+		print "Written %d bytes to %s." % (len(dat), file)
+		return True
 
 def write_Note(file, channel, note, bpm = 120, repeat = 0):
 	"""Expects a Note object from mingus.containers and \
