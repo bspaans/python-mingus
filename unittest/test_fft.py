0dfb3f9dde5cbdc0e19debe572872b9714dad548
Rhijnauwen <devnull@localhost>
2009-06-26 00:59:18 +0000

    First unit test for the fft module.

--- unittest/test_fft.py
+++ unittest/test_fft.py
@@ -10,7 +10,10 @@ from mingus.containers import *
 class test_fft(unittest.TestCase):
 
         def setUp(self):
-                pass
+                self.data, self.freq, self.bits = fft.data_from_file("440_sine_clean.wav")
+
+        def test_is_most_likely(self):
+                self.assertEqual(Note("A"), fft.is_most_likely(self.data, self.freq, self.bits))
 
 def suite():
         return unittest.TestLoader().loadTestsFromTestCase(test_fft)
