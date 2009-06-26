1f95bf2dbc88ff9bd21797b74b35df08a3f1ce89
Rhijnauwen <devnull@localhost>
2009-06-26 01:04:50 +0000

    Finished fft unit tests for now.

--- unittest/test_fft.py
+++ unittest/test_fft.py
@@ -12,8 +12,11 @@ class test_fft(unittest.TestCase):
         def setUp(self):
                 self.data, self.freq, self.bits = fft.data_from_file("440_sine_clean.wav")
 
-        def test_is_most_likely(self):
-                self.assertEqual(Note("A"), fft.is_most_likely(self.data, self.freq, self.bits))
+        def test_find_Note(self):
+                self.assertEqual(Note("A"), fft.find_Note(self.data, self.freq, self.bits))
+
+        def test_find_melody(self):
+                self.assertEqual( [(Note("A-4"), 86), (Note("A-5"), 86)], fft.find_melody("440_880_clean.wav", 512)[:2])
 
 def suite():
         return unittest.TestLoader().loadTestsFromTestCase(test_fft)
