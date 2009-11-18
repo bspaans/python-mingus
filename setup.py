480283af036b8f65629b045781ed061c6c63bb45
Rhijnauwen <devnull@localhost>
2009-11-18 07:50:03 +0000

    Fixed whitespace in setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name= "mingus",
-	  version = "0.4.3.0",
-	  description = "mingus is a music package for Python",
+      version = "0.4.3.0",
+      description = "mingus is a music package for Python",
 
-          long_description = 
+      long_description = 
 """mingus is a package for Python used by programmers, musicians, \
 composers and researchers to make and investigate music. At the core of mingus is music theory, \
 which includes topics like intervals, chords, scales and progressions. These components are 
@@ -19,31 +19,31 @@ APIs and has a tutorial making it easy to jump straight in.
 http://mingus.googlecode.com
 """,
 
-	  author = "Bart Spaans",
-	  author_email = "onderstekop@gmail.com",
-	  url = "http://mingus.googlecode.com/",
-	  packages = ['mingus', 'mingus.core', 'mingus.containers', 'mingus.extra', 'mingus.midi'],
-	  data_files = [('mingus_examples/pygame-drum', ['mingus_examples/pygame-drum/pad.png',\
-			  		     'mingus_examples/pygame-drum/pygame-drum.py']),\
-			('mingus_examples/pygame-piano', ['mingus_examples/pygame-piano/pygame-piano.py',
-				 			   'mingus_examples/pygame-piano/keys.png'])],
-          license="GPLv3",
-	  classifiers = [
-			'Intended Audience :: Developers',
-			'Intended Audience :: Science/Research',
-			'Intended Audience :: Other Audience',
-			'License :: OSI Approved :: GNU General Public License (GPL)',
-			'Operating System :: OS Independent',
-			'Programming Language :: Python',
-			'Topic :: Artistic Software',
-			'Topic :: Education',
-			'Topic :: Multimedia',
-			'Topic :: Multimedia :: Graphics :: Presentation',
-			'Topic :: Multimedia :: Sound/Audio',
-			'Topic :: Multimedia :: Sound/Audio :: MIDI',
-			'Topic :: Multimedia :: Sound/Audio :: Analysis',
-			'Topic :: Scientific/Engineering :: Information Analysis',
-			'Topic :: Scientific/Engineering :: Visualization',
-			'Topic :: Software Development :: Libraries :: Python Modules',
-			  ]
-	  )
+      author = "Bart Spaans",
+      author_email = "onderstekop@gmail.com",
+      url = "http://mingus.googlecode.com/",
+      packages = ['mingus', 'mingus.core', 'mingus.containers', 'mingus.extra', 'mingus.midi'],
+      data_files = [('mingus_examples/pygame-drum', ['mingus_examples/pygame-drum/pad.png',\
+                         'mingus_examples/pygame-drum/pygame-drum.py']),\
+            ('mingus_examples/pygame-piano', ['mingus_examples/pygame-piano/pygame-piano.py',
+                               'mingus_examples/pygame-piano/keys.png'])],
+      license="GPLv3",
+      classifiers = [
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Other Audience',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Artistic Software',
+            'Topic :: Education',
+            'Topic :: Multimedia',
+            'Topic :: Multimedia :: Graphics :: Presentation',
+            'Topic :: Multimedia :: Sound/Audio',
+            'Topic :: Multimedia :: Sound/Audio :: MIDI',
+            'Topic :: Multimedia :: Sound/Audio :: Analysis',
+            'Topic :: Scientific/Engineering :: Information Analysis',
+            'Topic :: Scientific/Engineering :: Visualization',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+              ]
+      )
