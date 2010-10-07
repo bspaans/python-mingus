0ffc2b40cddb14c1f946a3e614f460fd1b938bf9
Carlo Stemberger <devnull@localhost>
2010-10-07 22:30:21 +0200

    function get_key implemented

--- mingus/core/keys.py
+++ mingus/core/keys.py
@@ -27,7 +27,7 @@
 ================================================================================
 """
 
-#from mt_exceptions import NoteFormatError, RangeError
+from mt_exceptions import FormatError, RangeError
 #import intervals
 import notes
 
@@ -36,25 +36,37 @@ def is_valid_key(key):
 
     return notes.is_valid_note(key.upper())
 
-sharp_keys = [
-        ('C', 'a'),   # 0
-        ('G', 'e'),   # 1
-        ('D', 'b'),   # 2
-        ('A', 'f#'),  # 3
-        ('E', 'c#'),  # 4
-        ('B', 'g#'),  # 5
-        ('F#', 'd#'), # 6
-        ('C#', 'a#')  # 7
+keys = [
+        ('Cb', 'ab'), #  7 b
+        ('Gb', 'eb'), #  6 b
+        ('Db', 'bb'), #  5 b
+        ('Ab', 'f'),  #  4 b
+        ('Eb', 'c'),  #  3 b
+        ('Bb', 'g'),  #  2 b
+        ('F', 'd'),   #  1 b
+        ('C', 'a'),   #  nothing
+        ('G', 'e'),   #  1 #
+        ('D', 'b'),   #  2 #
+        ('A', 'f#'),  #  3 #
+        ('E', 'c#'),  #  4 #
+        ('B', 'g#'),  #  5 #
+        ('F#', 'd#'), #  6 #
+        ('C#', 'a#')  #  7 #
         ]
 
-flat_keys = [
-        ('C', 'a'),   # 0
-        ('F', 'd'),   # 1
-        ('Bb', 'g'),  # 2
-        ('Eb', 'c'),  # 3
-        ('Ab', 'f'),  # 4
-        ('Db', 'bb'), # 5
-        ('Gb', 'eb'), # 6
-        ('Cb', 'ab')  # 7
-        ]
+def get_key(number=0, token=''):
+    """Return the tuple containing the major key corrensponding to the
+    accidentals put as input, and his relative minor."""
 
+    if number not in range(8):
+        raise RangeError, 'Integer not in range 0-7.'
+    if token == 'b':
+        couple = 7 - number
+    elif token == "#":
+        couple = 7 + number
+    elif token == '' and number == 0:
+        couple = 7
+    else:
+        raise FormatError, "'%s' unrecognized: only 'b' and '#' admitted"\
+                % token
+    return keys[couple]
