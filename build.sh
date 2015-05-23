4744bdc45141fdcf59bbfe2990e2917a7c902421
Bart Spaans <bart.spaans@gmail.com>
2015-05-23 06:25:52 +0100

    simpler build. no messing with git history

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ git rm -rf doc/ genindex.html _images/ index.html _modules/ objects.inv py-modin
 git checkout sphinx-workspace sphinx_build/html
 mv sphinx_build/html/* .
 rm -rf sphinx_build
-git add .
+git add doc/ genindex.html _images/ index.html _modules/ objects.inv py-modindex.html search.html searchindex.js _sources/ _static/
 git commit -m "Scripted build"
 git push origin gh-pages
