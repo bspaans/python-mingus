4ffb28ab51c9eb773fb898fa2dd87555832a3474
Bart Spaans <bart.spaans@gmail.com>
2015-05-23 06:36:07 +0100

    Build sphinx documentation in one branch and then check out one folder in another. God help us all.

--- scripts/build_github_pages.sh
+++ scripts/build_github_pages.sh
@@ -4,10 +4,11 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd "$DIR/../"
+git checkout sphinx-workspace
+./build.sh
+
 git checkout gh-pages
 ./build.sh
-git add .
-git commit -m "Scripted build"
-git push origin gh-pages
+
 git checkout "$CURRENT_BRANCH"
 
