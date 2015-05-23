798e16f29c21871a6a576bbc07c43df42c294b8b
Bart Spaans <bart.spaans@gmail.com>
2015-05-23 06:27:16 +0100

    delete

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 git rm -rf doc/ genindex.html _images/ index.html _modules/ objects.inv py-modindex.html search.html searchindex.js _sources/ _static/
 git checkout sphinx-workspace sphinx_build/html
 mv sphinx_build/html/* .
-rm -rf sphinx_build
+git rm -rf sphinx_build
 git add doc/ genindex.html _images/ index.html _modules/ objects.inv py-modindex.html search.html searchindex.js _sources/ _static/
 git commit -m "Scripted build"
 git push origin gh-pages
