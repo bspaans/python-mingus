eed99975a0a6b0dd38e6efc48b33980977099f63
bspaans <onderstekop@gmail.com>
2015-09-03 21:11:37 +0200

    supporting all the old chords again

--- tests/test_chords.py
+++ tests/test_chords.py
@@ -30,6 +30,58 @@ def test_Chord_augmented_triad():
     chord = Chords.augmented_triad('C4')
     _chord_tester(chord, ['C4', 'E4', 'G#4'])
 
+def test_Chord_augmented_minor_seventh():
+    chord = Chords.augmented_minor_seventh('C4')
+    _chord_tester(chord, ['C4', 'E4', 'G#4', 'Bb4'])
+
+def test_Chord_augmented_major_seventh():
+    chord = Chords.augmented_major_seventh('C4')
+    _chord_tester(chord, ['C4', 'E4', 'G#4', 'B4'])
+
+def test_Chord_suspended_triad():
+    chord = Chords.suspended_triad('C4')
+    _chord_tester(chord, ['C4', 'F4', 'G4'])
+
+def test_Chord_suspended_fourth_triad():
+    chord = Chords.suspended_fourth_triad('C4')
+    _chord_tester(chord, ['C4', 'F4', 'G4'])
+
+def test_Chord_suspended_second_triad():
+    chord = Chords.suspended_second_triad('C4')
+    _chord_tester(chord, ['C4', 'D4', 'G4'])
+
+def test_Chord_suspended_seventh():
+    chord = Chords.suspended_seventh('C4')
+    _chord_tester(chord, ['C4', 'F4', 'G4', 'Bb4'])
+
+def test_Chord_suspended_fourth_ninth():
+    chord = Chords.suspended_fourth_ninth('C4')
+    _chord_tester(chord, ['C4', 'F4', 'G4', 'Db5'])
+
+def test_Chord_eleventh():
+    chord = Chords.eleventh('C4')
+    _chord_tester(chord, ['C4', 'G4', 'Bb4', 'F5'])
+
+def test_Chord_minor_eleventh():
+    chord = Chords.minor_eleventh('C4')
+    _chord_tester(chord, ['C4', 'Eb4', 'G4', 'Bb4', 'F5'])
+
+def test_Chord_lydian_dominant_seventh():
+    chord = Chords.lydian_dominant_seventh('C4')
+    _chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'F#5'])
+
+def test_Chord_minor_thirteenth():
+    chord = Chords.minor_thirteenth('C4')
+    _chord_tester(chord, ['C4', 'Eb4', 'G4', 'Bb4', 'D5', 'A5'])
+
+def test_Chord_major_thirteenth():
+    chord = Chords.major_thirteenth('C4')
+    _chord_tester(chord, ['C4', 'E4', 'G4', 'B4', 'D5', 'A5'])
+
+def test_Chord_dominant_thirteenth():
+    chord = Chords.dominant_thirteenth('C4')
+    _chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'D5', 'A5'])
+
 def test_Chord_major_seventh():
     chord = Chords.major_seventh('C4')
     _chord_tester(chord, ['C4', 'E4', 'G4', 'B4'])
@@ -41,6 +93,10 @@ def test_Chord_minor_seventh():
 def test_Chord_dominant_seventh():
     chord = Chords.dominant_seventh('C4')
     _chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4'])
+
+def test_Chord_diminished_seventh():
+    chord = Chords.diminished_seventh('C4')
+    _chord_tester(chord, ['C4', 'Eb4', 'Gb4', 'Bbb4'])
 
 def test_Chord_half_diminished_seventh():
     chord = Chords.half_diminished_seventh('C4')
@@ -82,3 +138,14 @@ def test_Chord_dominant_flat_ninth():
     chord = Chords.dominant_flat_ninth('C4')
     _chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'Db5'])
 
+def test_Chord_dominant_sharp_ninth():
+    chord = Chords.dominant_sharp_ninth('C4')
+    _chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'D#5'])
+
+def test_Chord_dominant_flat_five():
+    chord = Chords.dominant_flat_five('C4')
+    _chord_tester(chord, ['C4', 'E4', 'Gb4', 'Bb4'])
+
+def test_Chord_hendrix_chord():
+    chord = Chords.hendrix_chord('C4')
+    _chord_tester(chord, ['C4', 'E4', 'G4', 'Bb4', 'Eb5'])
