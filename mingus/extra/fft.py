3291c55c0b638fad09e6de8c68bbf7050511df2a
Eduardo Dobay <edudobay@gmail.com>
2019-11-10 16:31:18 -0300

    Use math.ceil to automatically convert to int
    In the slice `[0:uniquePts]`, uniquePts cannot be a floating-point
    number. We could round the result of `numpy.ceil` but I can't see a
    reason why we would need the _ceil_ function from numpy; instead I used
    the built-in, which returns an integer without need for further
    conversion.

--- mingus/extra/fft.py
+++ mingus/extra/fft.py
@@ -24,6 +24,7 @@ This module can also convert the found frequencies to Note objects.
 """
 from __future__ import absolute_import
 
+import math
 import wave
 import struct
 import numpy
@@ -96,7 +97,7 @@ def find_frequencies(data, freq=44100, bits=16):
     # Fast fourier transform
     n = len(data)
     p = _fft(data)
-    uniquePts = numpy.ceil((n + 1) / 2.0)
+    uniquePts = math.ceil((n + 1) / 2.0)
 
     # Scale by the length (n) and square the value to get the amplitude
     p = [(abs(x) / float(n)) ** 2 * 2 for x in p[0:uniquePts]]
